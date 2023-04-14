4e47e6419695e6f89f20ada8bc39d551be70b3e5
Shams425 <alexdavid486@gmail.com>
2023-04-14 07:29:39 +0300

    making the first action in Redux

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello from index.js");
+// making the first action in redux
+const BUY_CAKE = "BUY_CAKE";
+
+//making an action creater
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "first redux action",
+  };
+}
